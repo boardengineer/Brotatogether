79fbf41882c5fc57f36bea2347909257ac52769e
boardengineer <77598527+boardengineer@users.noreply.github.com>
2023-06-30 09:37:22 -0700

    Fix Vs Mode

--- Pasha-Brotatogether/extensions/weapons/shooting_behaviors/melee_weapon_shooting_behavior.gd
+++ Pasha-Brotatogether/extensions/weapons/shooting_behaviors/melee_weapon_shooting_behavior.gd
@@ -3,9 +3,10 @@ extends MeleeWeaponShootingBehavior
 func shoot(distance:float)->void :
 	if  $"/root".has_node("GameController"):
 		var game_controller = $"/root/GameController"
-		for player_id in game_controller.tracked_players:
-			var player = game_controller.tracked_players[player_id]["player"]
-			for weapon_index in player.current_weapons.size():
-				if player.current_weapons[weapon_index] == _parent:
-					game_controller.send_shot(player_id, weapon_index)
+		if game_controller and game_controller.game_mode == "shared":
+			for player_id in game_controller.tracked_players:
+				var player = game_controller.tracked_players[player_id]["player"]
+				for weapon_index in player.current_weapons.size():
+					if player.current_weapons[weapon_index] == _parent:
+						game_controller.send_shot(player_id, weapon_index)
 	.shoot(distance)
--- Pasha-Brotatogether/extensions/weapons/shooting_behaviors/ranged_weapon_shooting_behavior.gd
+++ Pasha-Brotatogether/extensions/weapons/shooting_behaviors/ranged_weapon_shooting_behavior.gd
@@ -5,11 +5,12 @@ func shoot(_distance:float)->void :
 		var game_controller = $"/root/GameController"
 		if game_controller and game_controller.game_mode == "shared" and not game_controller.is_source_of_truth:
 			return
-		for player_id in game_controller.tracked_players:
-			var player = game_controller.tracked_players[player_id]["player"]
-			for weapon_index in player.current_weapons.size():
-				if player.current_weapons[weapon_index] == _parent:
-					game_controller.send_shot(player_id, weapon_index)
+		elif game_controller and game_controller.game_mode == "shared":
+			for player_id in game_controller.tracked_players:
+				var player = game_controller.tracked_players[player_id]["player"]
+				for weapon_index in player.current_weapons.size():
+					if player.current_weapons[weapon_index] == _parent:
+						game_controller.send_shot(player_id, weapon_index)
 	.shoot(_distance)
 	
 func shoot_projectile(rotation:float = _parent.rotation, knockback:Vector2 = Vector2.ZERO)->void :
