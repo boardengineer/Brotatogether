20a7be7514c500f7d3da22943457b54d6a707865
boardengineer <77598527+boardengineer@users.noreply.github.com>
2024-01-24 20:15:23 -0800

    Fix crash on random weapon select

--- Pasha-Brotatogether/extensions/ui/menus/run/weapon_selection.gd
+++ Pasha-Brotatogether/extensions/ui/menus/run/weapon_selection.gd
@@ -4,21 +4,24 @@ func on_element_pressed(element:InventoryElement)->void :
 	if weapon_added:
 		return 
 	
+	var item
 	if element.is_random:
 		weapon_added = true
-		var _weapon = RunData.add_weapon(Utils.get_rand_element(available_elements), true)
+		item = Utils.get_rand_element(available_elements)
+		var _weapon = RunData.add_weapon(item, true)
 	elif element.is_special:
 		return 
 	else :
 		weapon_added = true
-		var _weapon = RunData.add_weapon(element.item, true)
+		item = element.item
+		var _weapon = RunData.add_weapon(item, true)
 	
 	RunData.add_starting_items_and_weapons()
 	
 	if $"/root/GameController":
 		var game_controller = $"/root/GameController"
 		if game_controller.back_to_lobby:
-			game_controller.on_weapon_selected(element.item.my_id)
+			game_controller.on_weapon_selected(item.my_id)
 			var _error = get_tree().change_scene("res://mods-unpacked/Pasha-Brotatogether/ui/multiplayer_lobby.tscn")
 			return
 	
