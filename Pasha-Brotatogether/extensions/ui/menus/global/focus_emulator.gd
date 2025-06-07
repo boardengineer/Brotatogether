fa3e59db71ad70d63f70cca003f851483a774ef6
boardengineer <77598527+boardengineer@users.noreply.github.com>
2025-06-07 14:31:59 -0700

    Hardcode R and L as shop tab switch

--- Pasha-Brotatogether/extensions/ui/menus/global/focus_emulator.gd
+++ Pasha-Brotatogether/extensions/ui/menus/global/focus_emulator.gd
@@ -1,9 +1,14 @@
 extends "res://ui/menus/global/focus_emulator.gd"
 
 var global_focused_control
+var steam_connection
+var brotatogether_options
 
 
 func ready() -> void:
+	steam_connection = $"/root/SteamConnection"
+	brotatogether_options = $"/root/BrotogetherOptions"
+	
 	var _err = get_viewport().connect("gui_focus_changed", self, "_on_focus_changed_multiplayer")
 
 
@@ -14,6 +19,21 @@ func _on_focus_changed_multiplayer(control:Control) -> void:
 
 # Have line edit eat inputs so that you can send messages
 func _handle_input(event:InputEvent) -> bool:
+	if not brotatogether_options:
+		brotatogether_options = $"/root/BrotogetherOptions"
+	
+	if not steam_connection:
+		steam_connection = $"/root/SteamConnection"
+	
+	if get_tree().current_scene.name == "CoopShop" and brotatogether_options.in_multiplayer_game:
+		if event is InputEventKey:
+			if event.pressed:
+				var carousel = get_tree().current_scene._get_coop_player_container(steam_connection.get_my_index()).carousel
+				if event.scancode == KEY_R: 
+					carousel._on_ArrowRight_pressed()
+				if event.scancode == KEY_L: 
+					carousel._on_ArrowLeft_pressed()
+	
 	if global_focused_control != null and global_focused_control is LineEdit:
 		return false
 	return ._handle_input(event)
