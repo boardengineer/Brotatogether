0f68241dfbd493472f764faa2201068aeed653f4
boardengineer <77598527+boardengineer@users.noreply.github.com>
2025-06-20 11:43:27 -0700

    Mock out shop items deactivating

---
+++ Pasha-Brotatogether/extensions/ui/menus/shop/shop_item.gd
@@ -0,0 +1,27 @@
+extends "res://ui/menus/shop/shop_item.gd"
+
+var steam_connection
+var brotatogether_options
+
+var in_multiplayer_game = false
+
+func _ready():
+	steam_connection = $"/root/SteamConnection"
+	brotatogether_options = $"/root/BrotogetherOptions"
+	in_multiplayer_game = brotatogether_options.in_multiplayer_game
+
+
+func deactivate()->void :
+	if in_multiplayer_game:
+		if steam_connection.is_host():
+			.deactivate()
+			return
+		else:
+			modulate = Color(1, 1, 1, 0)
+			_button.disable()
+			_lock_icon.hide()
+			locked = false
+			active = false
+			emit_signal("shop_item_deactivated", self)
+	else:
+		.deactivate()
